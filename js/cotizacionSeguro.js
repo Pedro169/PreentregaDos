a58a4e3e93c74f9af2fdac0f078eab45fe85c5f0
pedro169 <pedro.tapiacast@gmail.com>
2024-01-03 17:13:52 -0400

    libreria

--- js/cotizacionSeguro.js
+++ js/cotizacionSeguro.js
@@ -4,6 +4,7 @@ const coberturaSeguro = document.querySelector("span.descripcionSeguro");
 const totalIva = document.querySelector("span.valorTotalSeguro");
 const insertaNombre = document.querySelector("span.nombre");
 const insertaNumeroTelefonico = document.querySelector("span.telefono");
+const buttonCotizar = document.querySelector("#contratar");
 
 const recuperarDatosSeguro = () => {
   const datosSeguroLs = JSON.parse(localStorage.getItem("DatosSeguro"));
@@ -16,14 +17,25 @@ const recuperarDatosSeguro = () => {
   insertaNumeroTelefonico.textContent = datosSeguroLs.celular;
 };
 
-document
-  .getElementById("mostrarMensaje")
-  .addEventListener("click", function () {
-    document.getElementById("mensaje").classList.remove("oculto");
+// document
+//   .getElementById("mostrarMensaje")
+//   .addEventListener("click", function () {
+//     document.getElementById("mensaje").classList.remove("oculto");
+//   });
+// document.getElementById("cerrarMensaje").addEventListener("click", function () {
+//   document.getElementById("mensaje").classList.add("oculto");
+//   location.href = "index.html";
+// });
+
+buttonCotizar.addEventListener("click", ()=> {
+  let telefono = insertaNumeroTelefonico.textContent;
+  let seguro = nombreSeguro.textContent;
+  Swal.fire({
+    title: "Gracias por contratar nuestro " + seguro,
+    text: "nos comunicaremos contigo al celular " + telefono , 
+    icon: "success"
   });
-document.getElementById("cerrarMensaje").addEventListener("click", function () {
-  document.getElementById("mensaje").classList.add("oculto");
-  location.href = "index.html";
-});
+})
+
 
 recuperarDatosSeguro();
